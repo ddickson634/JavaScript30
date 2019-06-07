f63d4a4d82a161415aafc4fbdb944fdcec598fdc
ddickson634 <ddickson634@gmail.com>
2019-06-07 08:27:23 -0400

    Got hour hand to log to console

--- 02_JS_CSS_Clock/index.js
+++ 02_JS_CSS_Clock/index.js
@@ -13,6 +13,12 @@ function setDate() {
     const minsDegrees = ((mins / 60) * 360) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
     
+     
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(hour, mins, seconds,)
+
 }
 
 setInterval(setDate, 1000);
